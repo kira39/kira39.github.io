e9625dbe7352c61ae30d61d391d34c0a5c2e25fc
Emily Chen <Emily@Emilys-MBP-5.wireless.yale.internal>
2017-04-14 00:27:38 -0400

    website up and running and basic forms exist

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_URL);
 const Users = require('./models/users.js');
 const Tasks = require('./models/tasks.js');
 
+// var port = process.env.PORT || 4000;
+
 // Configure our app
 const store = new MongoDBStore({
   uri: process.env.MONGO_URL,
